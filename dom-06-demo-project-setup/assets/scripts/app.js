d83040f863ff0e803ef1d8c8237351d5cc9f46c5
hugogit120 <hugomercalibre@gmail.com>
2021-10-05 10:44:23 +0200

    creating a movie & clearing inputs

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -5,6 +5,8 @@ const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll("input");
 
+const movies = [];
+
 const toggleBackDrop = () => {
   backDrop.classList.toggle("visible");
 };
@@ -31,6 +33,22 @@ const addMovieHandler = () => {
     ratingValue > 5
   ) {
     alert("please enter valid values (rating between 1 and 5");
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+};
+
+const clearMovieInput = () => {
+  for (const usrInput of userInputs) {
+    usrInput.value = "";
   }
 };
 
